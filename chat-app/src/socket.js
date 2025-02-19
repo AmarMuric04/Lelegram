0ae08692d289b413ccd189ef8b4e59d431b56cd5
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2025-02-19 18:57:50 +0100

    changed socketio port

--- chat-app/src/socket.js
+++ chat-app/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_SERVER_URL = "http://localhost:3000";
+const SOCKET_SERVER_URL = import.meta.env.VITE_SERVER_PORT;
 
 export const socket = io(SOCKET_SERVER_URL, {
   autoConnect: false,
